5d6d652df5b3790d358fa0f2a5999252f12d0e87
Ibrahima TRAORE <ibtraore@e1r7p21.42.fr>
2017-05-31 16:52:52 +0200

    poy

---
+++ src/search.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibtraore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/31 16:15:54 by ibtraore          #+#    #+#             */
+/*   Updated: 2017/05/31 16:52:38 by ibtraore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/omen.h"
+
+
+int			search(double lat, double lon, char **tab)
+{
+	int i;
+	double _lat;
+	doubleble _lon;
+	char	*trim;
+	char *split[2];
+	double lat2;
+	double  lon2;
+	double flag1;
+	double flag2;
+	int conddtinu;
+	int id;
+
+	i = 0;
+	id = 0;
+	continu = 1;
+	while (tab[i])
+	{
+		flag1 = 0;
+		flag2 = 0;
+		trim = ft_strtrim(tab[i]);
+		if ((ft_strcmp(trim , "\"id\""), 4) == 0)
+			id = ft_atoi(trim);
+		if ((ft_strncmp(trim, "\"lat\"") ,4) == 0) 
+		{
+			split = ft_strsplit(trim, ' ');
+			lat2 = ft_atof(split[1]);
+			if (abs((int)lat2 - lat) <= 1)
+				flag1 = 1;
+		}
+		if ((ft_strncmp(trim, "\"lon\"") ,4) == 0) 
+		{
+			split = ft_strsplit(trim, ' ');
+			lat2 = ft_atof(split[1]);
+			if (abs((int)lat2 - lon) <= 1)
+				flag2 = 1;
+		}
+		ft_strdel(&trim);
+		if (flag1 && flag2)
+			return (id);
+		i++;
+	}
+	return (0);
+}
+
+int			is_bulding(int id,  t_bulding *list, char **tab)
+{
+		t_bulding *tmp;
+
+		tmp = list;
+		while (tmp && id)
+		{
+			if (0 != id)
+				return (TRUE);
+			tmp = tmp->next;
+		}
+		return (TFALSE);
+}
